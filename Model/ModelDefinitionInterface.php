19310c44549643e54cf79e961d382019d2fad900
Mark Sprotte <sprotte@eventit.ag>
2019-01-11 08:49:54 +0100

    added global search stuff for datatables

--- Model/ModelDefinitionInterface.php
+++ Model/ModelDefinitionInterface.php
@@ -4,4 +4,15 @@ namespace Sg\DatatablesBundle\Model;
 
 interface ModelDefinitionInterface
 {
+    /**
+     * @return bool
+     */
+    public function hasSearch(): bool;
+
+    /**
+     * @param array $search
+     *
+     * @return self
+     */
+    public function setSearch(array $search): self;
 }
